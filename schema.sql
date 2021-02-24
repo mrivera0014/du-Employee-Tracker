91c9c1cf4522450cf881920bbb31e8f6e22710a9
Marble <mrivera0014@gmail.com>
2021-02-23 19:19:17 -0700

    created sql file,database, and tables

---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employees_DB;
+
+CREATE DATABASE employees_DB;
+
+USE employees_DB;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL ,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(6,2) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT NOT NULL ,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+SELECT * FROM employees_DB
